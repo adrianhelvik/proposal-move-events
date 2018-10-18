30050ab59c2da08f03fdad408fbfc7138d780115
Adrian Helvik <adrian.helvik@specifique.no>
2018-10-18 15:34:25 +0200

    Added possibility to supply a containerElement to createSnapshot and set the z-index for snapshots to be the highest possible z-index

--- src/snapshot.js
+++ src/snapshot.js
@@ -4,23 +4,31 @@ export function polyfill() {
   document.createSnapshot = createSnapshot
 }
 
-export function createSnapshot(element) {
-  return new Snapshot(element)
+export function createSnapshot(element, containerElement = document.body) {
+  return new Snapshot(element, containerElement)
 }
 
 class Snapshot {
-  constructor(element) {
+  constructor(element, containerElement) {
+    this.containerElement = containerElement
     this.element = element
     this.clone = cloneNode(this.element)
     this.clone.style.position = 'fixed'
     this.clone.style.left = 0
     this.clone.style.top = 0
     this.clone.style.pointerEvents = 'none'
+
+    // We use the largest z-index. Mount order should
+    // determine the layering of snapshots. They should
+    // overlay any other DOM element.
+    //
+    // Reference: http://softwareas.com/whats-the-maximum-z-index/
+    this.clone.style.zIndex = 2147483647
   }
 
   place({ x, y }) {
     this.move({ x, y })
-    document.body.appendChild(this.clone)
+    this.containerElement.appendChild(this.clone)
   }
 
   move({ x, y, transition = 0 }) {
@@ -29,6 +37,6 @@ class Snapshot {
   }
 
   remove() {
-    this.clone.remove()
+    this.clone.parentNode.removeChild(this.clone)
   }
 }
